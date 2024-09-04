82fd7a36bc4323b14007745ee3446c246c153113
1TAEKIM <oneteak1@gmail.com>
2024-09-04 18:16:36 +0900

    1018 trying

---
+++ 100_baekjoon/12_브루트 포스/1018_체스판 다시 칠하기.py
@@ -0,0 +1,54 @@
+n, m = map(int, input().split())
+
+arr = []
+for i in range(n):
+    row = input()
+    arr.append(list(row))
+
+print(arr[0])
+tar = []
+for i in range(n - 7):
+    for j in range(m - 7):
+        tar.append(arr[i][j])
+
+print(tar)
+# cnt = 0
+# if arr[0][0] == 'W':
+#     for i in range(n):
+#         for j in range(m):
+#             if i % 2 == 0 and j % 2 == 0:
+#                 if arr[i][j] != 'W':
+#                     cnt += 1
+#
+#             elif i % 2 == 1 and j % 2 == 1:
+#                 if arr[i][j] != 'W':
+#                     cnt += 1
+#
+#             elif i % 2 == 0 and j % 2 == 1:
+#                 if arr[i][j] != 'B':
+#                     cnt += 1
+#
+#             elif i % 2 == 1 and j % 2 == 0:
+#                 if arr[i][j] != 'B':
+#                     cnt += 1
+#
+# else:
+#     for i in range(n):
+#         for j in range(m):
+#             if i % 2 == 0 and j % 2 == 0:
+#                 if arr[i][j] != 'B':
+#                     cnt += 1
+#
+#             elif i % 2 == 1 and j % 2 == 1:
+#                 if arr[i][j] != 'B':
+#                     cnt += 1
+#
+#             elif i % 2 == 0 and j % 2 == 1:
+#                 if arr[i][j] != 'W':
+#                     cnt += 1
+#
+#             elif i % 2 == 1 and j % 2 == 0:
+#                 if arr[i][j] != 'W':
+#                     cnt += 1
+#
+# print(cnt)
