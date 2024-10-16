940c2b3649df07bb777023d4b2eeb9feee6fafe0
1TAEKIM <oneteak1@gmail.com>
2024-10-16 14:54:57 +0900

    10814 trying

--- 100_baekjoon/13_정렬/10814_나이순 정렬.py
+++ 100_baekjoon/13_정렬/10814_나이순 정렬.py
@@ -1,18 +1,12 @@
 n = int(input())
 
 arr = []
-for _ in range(n):
+eql = []
+for i in range(n):
     tmp = list(input().split())
     arr.append(tmp)
+    if arr[i - 1][0] == arr[i][0]:
+       eql.append(tmp)
 
-tar = sorted(arr)
-# for i in range(n - 1):
-#     for j in range(i, n):
-#         if tar[i][0] > tar[j][0]:
-#             tar[i], tar[j] = tar[j], tar[i]
+arr = sorted(arr)
 
-# 만약 나이가 같을 때 tar arr 순서 다르면 arr 대로 바꾸기
-
-
-# 자기소개서, 포트폴리오, 영어 성적까지 준비 하기 어렵다,,
-# 그렇지만 좋은 날이 올 것이라 확신한다:)
