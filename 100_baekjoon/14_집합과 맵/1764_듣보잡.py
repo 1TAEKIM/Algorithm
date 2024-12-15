bf4607be51fae8842dc2b64cb0079106a7ec172e
1TAEKIM <oneteak1@gmail.com>
2024-12-15 18:10:24 +0900

    1764 trying

---
+++ 100_baekjoon/14_집합과 맵/1764_듣보잡.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+
+arr = []
+for i in range(n):
+    arr.append(input())
+
+tar = []
+for i in range(m):
+    tar.append(input())
+
+ans = []
+cnt = 0
+for i in range(m):
+    if tar[i] in arr:
+        ans.append(tar[i])
+        cnt += 1
+
+ans.sort()
+print(cnt)
+for i in range(len(ans)):
+    print(ans[i])
+
