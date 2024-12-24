95fbc31bec73d636940204067461d1ae3506a780
1TAEKIM <oneteak1@gmail.com>
2024-12-24 15:20:40 +0900

    10815 ok_cp

--- 100_baekjoon/14_집합과 맵/10815_숫자 카드.py
+++ 100_baekjoon/14_집합과 맵/10815_숫자 카드.py
@@ -1,9 +1,17 @@
-import sys
+n = int(input())
+n_arr = list(map(int, input().split()))
 
-n = int(sys.stdin.readline())
-n_arr = list(map(int, sys.stdin.readline().split()))
+m = int(input())
+m_arr = list(map(int, input().split()))
 
-m = int(sys.stdin.readline())
-m_arr = list(map(int, sys.stdin.readline().split()))
+# dict가 비교할 때 list보다 훨씬 빠름!!
+tmp = {}
 
+for i in range(n):
+    tmp[n_arr[i]] = 0
 
+for i in range(m):
+    if m_arr[i] in tmp:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
