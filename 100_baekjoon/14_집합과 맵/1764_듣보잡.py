2ffb14df0b3f31b435cb506b1e9c5f2abaaa1bb7
1TAEKIM <oneteak1@gmail.com>
2024-12-26 15:26:38 +0900

    1764 ok

--- 100_baekjoon/14_집합과 맵/1764_듣보잡.py
+++ 100_baekjoon/14_집합과 맵/1764_듣보잡.py
@@ -1,24 +1,23 @@
+"""
+N개의 줄에 걸쳐 듣도 못한 사람의 이름과
+N+2째 줄부터 보도 못한 사람의 이름이 주어진다.
+"""
+# 듣보잡의 수와 그 명단을 사전순으로 출력
+
 n, m = map(int, input().split())
 
-arr = []
-for i in range(n):
-    arr.append(input())
-
-tar = []
-for i in range(m):
-    tar.append(input())
-
+_dict = {}
 ans = []
-cnt = 0
-for i in range(m):
-    if tar[i] in arr:
-        ans.append(tar[i])
-        cnt += 1
+for i in range(n + m):
+    name = input()
+    if name not in _dict:
+        _dict[name] = 0
+    else:
+        _dict[name] = 1
+        ans.append(name)
 
 ans.sort()
-print(cnt)
+print(len(ans))
 for i in range(len(ans)):
     print(ans[i])
 
-
-# AWS SAA 시험 합격!!
