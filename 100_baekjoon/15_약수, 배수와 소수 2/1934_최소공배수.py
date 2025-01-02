1464bffcbc606245a7165e95046a1cfc6a0b132d
1TAEKIM <oneteak1@gmail.com>
2025-01-02 17:28:12 +0900

    1934 ok_cp

--- 100_baekjoon/15_약수, 배수와 소수 2/1934_최소공배수.py
+++ 100_baekjoon/15_약수, 배수와 소수 2/1934_최소공배수.py
@@ -1,3 +1,10 @@
+def euclidean(a, b):
+    while b != 0:
+        r = a % b
+        a = b
+        b = r
+    return a
+
 t = int(input())
 
 arr = []
@@ -5,12 +12,7 @@ for i in range(t):
     a, b = map(int, input().split())
     arr.append([a, b])
 
-    while b != 0:
-        r = a % b
-        a = b
-        b = r
-
-    print(a)
-
-
-# Project in progress
+# 유클리드 호제법 공부 필요!!
+for i in range(t):
+    gcd = euclidean(arr[i][0], arr[i][1])
+    print(arr[i][0] * arr[i][1] // gcd)
