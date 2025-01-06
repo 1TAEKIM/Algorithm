91f5925fa3cbf7cacda9df3e1df7db600799f2bb
1TAEKIM <oneteak1@gmail.com>
2025-01-06 12:48:00 +0900

    13909 timeout

---
+++ 100_baekjoon/15_약수, 배수와 소수 2/13909_창문 닫기.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+# 각 창문 번호의 약수의 개수가 짝수 0, 홀수 1
+arr = [] * n
+ans = 0
+for i in range(1, n + 1):
+    cnt = 0
+    for j in range(1, i + 1):
+        if i % j == 0:
+            cnt += 1
+
+    if cnt % 2 == 0:
+        arr.append(0)
+    else:
+        arr.append(1)
+        ans += 1
+
+print(ans)
