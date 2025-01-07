bf5ea81797d05106fa90898d3a167661b3d5d64d
1TAEKIM <oneteak1@gmail.com>
2025-01-07 18:19:28 +0900

    28278 trying

--- 100_baekjoon/16_스텍, 큐, 덱/28278_스택 2.py
+++ 100_baekjoon/16_스텍, 큐, 덱/28278_스택 2.py
@@ -1,26 +1,32 @@
 n = int(input())
 
 stack = []
-for i in range(n):
-    num = int(input())
-    if num == 1:
-        stack.append(num)
-    elif num == 2:
-        if len(stack) == 0:
-            print(-1)
+
+for _ in range(n):
+    num = input()
+
+    if num[0] == '1':
+        stack.append(int(num[-1]))
+
+    elif num == '2':
+        if len(stack) > 0:
+            # pop method는 시간 복잡도가 O(N)
+            print(stack.pop(-1))
         else:
-            for j in range(len(stack) - 1):
-                print(stack[j])
-    elif num == 3:
+            print(-1)
+
+    elif num == '3':
         print(len(stack))
-    elif num == 4:
+
+    elif num == '4':
         if len(stack) == 0:
             print(1)
         else:
             print(0)
-    else:
-        if len(stack) == 0:
-            print(-1)
-        else:
+
+    elif num == '5':
+        if len(stack) > 0:
             print(stack[-1])
+        else:
+            print(-1)
 
