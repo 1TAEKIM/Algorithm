d1cf1e3fcc642ed99811aed11458e25b6b6949f0
1TAEKIM <oneteak1@gmail.com>
2025-02-02 14:13:26 +0900

    11866 ok_cp

--- 100_baekjoon/16_스텍, 큐, 덱/11866_요세푸스 문제 0.py
+++ 100_baekjoon/16_스텍, 큐, 덱/11866_요세푸스 문제 0.py
@@ -1,1 +1,15 @@
-# Restart!!! LET'S GO!!
+n, k = map(int, input().split())
+
+queue = [i for i in range(1, n + 1)]
+
+idx = 0
+res = []
+while queue:
+    idx += k - 1    # k-1번째 인덱스까지 건너뛰기
+    if idx >= len(queue):   # 인덱스가 범위를 넘어갈 경우
+        idx %= len(queue)   # 나머지 연산을 통해 인덱스 계산
+    res.append(str(queue.pop(idx)))
+
+# .join은 str형이 필요
+print("<", ", ".join(res), ">", sep="")
+
