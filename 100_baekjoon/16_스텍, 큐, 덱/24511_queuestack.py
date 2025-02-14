a34ec163cd14da41b7a287b20490f1e4a3a769ce
1TAEKIM <oneteak1@gmail.com>
2025-02-14 17:19:54 +0900

    24511 trying

---
+++ 100_baekjoon/16_스텍, 큐, 덱/24511_queuestack.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+
+a = [i for i in sys.stdin.readline().split()]
+
+deq = deque()
+stack = []
+
+b = [i for i in sys.stdin.readline().split()]
+
+m = int(sys.stdin.readline())
+c = [i for i in sys.stdin.readline().split()]
+
