613f20aec57d231ece22d2c5e1af830cd3bea078
1TAEKIM <oneteak1@gmail.com>
2025-02-15 15:19:56 +0900

    10989 ok_CP

--- 100_baekjoon/13_정렬/10989_수 정렬하기 3.py
+++ 100_baekjoon/13_정렬/10989_수 정렬하기 3.py
@@ -1,28 +1,44 @@
-#!!!! 계수(카운팅)정렬 이해 필요!! (아직 이해 X)
+# #!!!! 계수(카운팅)정렬 이해 필요!! (아직 이해 X)
+#
+# import sys
+#
+# n = int(sys.stdin.readline())
+#
+# arr = []
+# for i in range(n):
+#     num = int(sys.stdin.readline())
+#     arr.append(num)
+#
+# count_dict = {}
+#
+# for num in arr:
+#     if num in count_dict:
+#         count_dict[num] += 1
+#
+#     else:
+#         count_dict[num] = 1
+#
+# result = []
+# for num in range(max(arr) + 1):
+#     while num in count_dict and count_dict[num] != 0:
+#         result.append(num)
+#         count_dict[num] -= 1
+#
+# for i in range(len(result)):
+#     print(result[i])
+#
 
 import sys
 
 n = int(sys.stdin.readline())
 
-arr = []
-for i in range(n):
+count = [0] * 10001
+
+for _ in range(n):
     num = int(sys.stdin.readline())
-    arr.append(num)
+    count[num] += 1
 
-count_dict = {}
-
-for num in arr:
-    if num in count_dict:
-        count_dict[num] += 1
-
-    else:
-        count_dict[num] = 1
-
-result = []
-for num in range(max(arr) + 1):
-    while num in count_dict and count_dict[num] != 0:
-        result.append(num)
-        count_dict[num] -= 1
-
-for i in range(len(result)):
-    print(result[i])
+for i in range(1, 10001):
+    if count[i] != 0:
+        for _ in range(count[i]):
+            print(i)
