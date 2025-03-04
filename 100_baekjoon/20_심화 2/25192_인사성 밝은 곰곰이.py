a6e42f28c0e302315133937f4871b56d5d5b1a56
1TAEKIM <oneteak1@gmail.com>
2025-03-04 13:05:10 +0900

    25192 ok

--- 100_baekjoon/20_심화 2/25192_인사성 밝은 곰곰이.py
+++ 100_baekjoon/20_심화 2/25192_인사성 밝은 곰곰이.py
@@ -3,19 +3,20 @@ import sys
 n = int(sys.stdin.readline())
 
 arr = []
-cnt = 0
+ans = 0
 for i in range(n):
     name = sys.stdin.readline().strip()
     # enter 경우
     if name == 'ENTER':
+        ans += len(set(arr))
         arr = []
+
     # arr 이 비어있는 경우
     elif name != 'ENTER' and len(arr) == 0:
         arr.append(name)
-        cnt += 1
     else:
-        if name not in set(arr) and arr[-1] != name:
-            arr.append(name)
-            cnt += 1
+        arr.append(name)
 
-print(cnt)
+# 마지막 배열 안에 들어있는 원소 개수 더해주기
+ans += len(set(arr))
+print(ans)
