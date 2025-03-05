91948549fc12aba6cae58e9570258964dbba759a
1TAEKIM <oneteak1@gmail.com>
2025-03-05 23:07:37 +0900

    26069 trying

--- 100_baekjoon/20_심화 2/26069_붙임성 좋은 총총이.py
+++ 100_baekjoon/20_심화 2/26069_붙임성 좋은 총총이.py
@@ -2,13 +2,20 @@ import sys
 
 n = int(sys.stdin.readline())
 
-"""
-입력받을 때 배열 안에 있으면 카운트
-"""
-stack = []
-cnt = 1
+arr = []
+cnt = 0
 for i in range(n):
     a, b = sys.stdin.readline().split()
-    if stack[-1] == b:
-        cnt += 1
+    if len(arr) == 0:
+        arr.append(a)
+        arr.append(b)
+        cnt += 2
 
+    else:
+        if arr[-1] == a:
+            arr.append(b)
+            cnt += 1
+
+
+print()
+# 머릿 속 복잡
