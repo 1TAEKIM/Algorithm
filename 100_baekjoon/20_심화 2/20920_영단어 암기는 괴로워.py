07157c35d80ab36d17e4a31ac2ed450ca3e00ce7
1TAEKIM <oneteak1@gmail.com>
2025-03-08 14:58:49 +0900

    20920 ok_cp

--- 100_baekjoon/20_심화 2/20920_영단어 암기는 괴로워.py
+++ 100_baekjoon/20_심화 2/20920_영단어 암기는 괴로워.py
@@ -2,4 +2,25 @@
 1. 자주 나오는 단어
 2. 단어가 길수록
 3. 알파벳 사전 순
+반대로 해야지 최종 정렬
 """
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+dic = {}
+for i in range(n):
+    word = sys.stdin.readline().strip()
+    if len(word) < m:
+        continue
+    if word not in dic:
+        dic[word] = 1
+    else:
+        dic[word] += 1
+
+dic = sorted(dic.items(), key=lambda x:x[0])
+dic = sorted(dic, key=lambda x:len(x[0]), reverse=True)
+dic = sorted(dic, key=lambda x:x[1], reverse=True)
+
+for i in dic:
+    print(i[0])
